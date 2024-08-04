68f8e6e4f24a4c08af5b7e253aec28b5e01bcd86
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-08-04 15:06:27 +0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-     font-size : 30px;
-     font-weight : 600;
-`;
-
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-    background-color : orangered;
+    /* background-color : orangered; */
     padding: 20px;
 `;
 
@@ -19,9 +15,26 @@ export default function App() {
     <>
       <GlobalStyles/>
       <StyledApp>
-          <H1>The Wild Oasis</H1>
-          <Button>Check in</Button>
-          <Input type="number" placeholder="number of guests"/>
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+
+            <div>
+              <Heading as='h2'>Check in</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button variation='secondary' size='small' onClick={() => alert('Check out')}>Check out</Button>
+            </div>
+          </Row>
+  
+          <Row>
+            <Heading as='h3'>Form</Heading>
+          
+            <form>
+              <Input type="number" placeholder="number of guests"/>
+              <Input type="number" placeholder="number of guests"/>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   )
