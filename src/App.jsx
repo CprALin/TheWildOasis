7828bcd53ef1a57cc8ab0ad0edb94abf249811e6
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-08-04 11:31:39 +0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+     font-size : 30px;
+     font-weight : 600;
+`;
+
+
+const StyledApp = styled.div`
+    background-color : orangered;
+    padding: 20px;
+`;
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <GlobalStyles/>
+      <StyledApp>
+          <H1>The Wild Oasis</H1>
+          <Button>Check in</Button>
+          <Input type="number" placeholder="number of guests"/>
+      </StyledApp>
+    </>
   )
 }
 
