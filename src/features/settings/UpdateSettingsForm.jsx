d32a8f7ede9a224e76c9ac010f50f4df3bc9f504
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-09-01 19:32:09 +0300

    #

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,8 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import { useSettings } from './useSettings';
 import Spinner from '../../ui/Spinner';
+import { useUpdateSetting } from './useUpdateSettings';
+
 
 function UpdateSettingsForm() {
   const { isLoading , settings : { 
@@ -12,21 +14,30 @@ function UpdateSettingsForm() {
       breakfast_price
   } = {}} = useSettings();
 
+  const { isUpdating , updateSetting } = useUpdateSetting();
+
   if(isLoading) return <Spinner />;
+
+  function handleUpdate(e , field){
+      const { value } = e.target;
+
+      if(!value) return;
+      updateSetting({ [field] : value });
+  }
 
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' defaultValue={min_booking_length} />
+        <Input type='number' id='min-nights' disabled={isUpdating} defaultValue={min_booking_length} onBlur={e=>handleUpdate(e , "min_booking_length")}/>
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' defaultValue={max_booking_length}/>
+        <Input type='number' id='max-nights' disabled={isUpdating} defaultValue={max_booking_length} onBlur={e=>handleUpdate(e , "max_booking_length")}/>
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' defaultValue={max_guests_nr}/>
+        <Input type='number' id='max-guests' disabled={isUpdating} defaultValue={max_guests_nr} onBlur={e=>handleUpdate(e , "max_guests_nr")}/>
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' defaultValue={breakfast_price}/>
+        <Input type='number' id='breakfast-price' disabled={isUpdating} defaultValue={breakfast_price} onBlur={e=>handleUpdate(e , "breakfast_price")}/>
       </FormRow>
     </Form>
   );
