ec2d5cd84e67a47c238097d701a439c4d18301a0
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-09-22 12:59:18 +0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from "react-hot-toast";
 import Booking from "./ui/Booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({defaultOptions : {
     queries : {
@@ -29,7 +30,11 @@ export default function App() {
         <GlobalStyles/>
         <BrowserRouter>
               <Routes>
-                  <Route element={<AppLayout />}>
+                  <Route element={
+                    <ProtectedRoute>
+                        <AppLayout />
+                    </ProtectedRoute>
+                  }>
                     <Route index element={<Navigate replace to="Dashboard"/>}/>
                     <Route path="dashboard" element={<Dashboard />}/>
                     <Route path="bookings" element={<Bookings />}/>
