fa93ae9c6e39bba21932d221946aa6f5d565ebc0
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-09-23 21:10:07 +0300

    #

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,25 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { bookings , isLoading : isLoading1 } =  useRecentBookings();
+  const { stays , confirmedStays , isLoading : isLoading2 } = useRecentStays();
+
+  if(isLoading1 || isLoading2) return <Spinner />
+
+  
+
+  return (
+    <StyledDashboardLayout>
+        <div>Statistics</div>
+
+        <div>Today`s activity</div>
+
+        <div>Chart stay durations</div>
+
+        <div>Chart of sales</div>
+    </StyledDashboardLayout>
+  )
+}
+
