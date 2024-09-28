317e106e846d4f162a54d51b6d4667875567080b
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-09-28 14:48:38 +0300

    #

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,11 +1,19 @@
 import Button from "../../ui/Button";
+import PropTypes from "prop-types";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { isCheckingOut , checkout } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button $variation="primary" size="small" onClick={() => checkout} disabled={isCheckingOut}>
       Check out
     </Button>
   );
 }
 
 export default CheckoutButton;
+
+CheckoutButton.propTypes = {
+    bookingId : PropTypes.number
+}
